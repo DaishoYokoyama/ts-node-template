389cc35b1bcbcd8cbdc7a49bedd47c5f0b64026c
Daisho Yokoyama <daisyo.yokoyama@digitalhearts.com>
2021-12-13 10:39:10 +0900

    fix

--- src/index.ts
+++ src/index.ts
@@ -1,5 +1,21 @@
+import _ from 'lodash';
+import { stringify } from 'csv-stringify/sync';
+import { Crawler } from './crawler';
+import { Analyzer } from './analyzer';
+import * as utils from './util';
+
+const TARGET_URL = 'https://ncode.syosetu.com/n7308he/13/';
+// const TARGET_URL = 'https://www.wsj.com/';
+
 const main = async () => {
-  console.info('Hello');
+  const crawler = new Crawler(TARGET_URL);
+  const analyzer = new Analyzer();
+
+  const texts = await crawler.getParagraphTexts();
+  const results = stringify(_.flatMap(texts.map((x) => analyzer.analyze(x))), {
+    header: true,
+  });
+  utils.writeFile('./dist/analyzed.csv', results);
 };
 
 main();
